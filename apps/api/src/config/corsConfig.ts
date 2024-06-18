ada843defbe7f527b32fffa04c919db1cb37fb8f
Jennifer Nevarez <jennifer.nevarez@accenture.com>
2024-06-18 12:31:21 -0700

    fix(api): Remove Double Callback Execution

--- apps/api/src/config/corsConfig.ts
+++ apps/api/src/config/corsConfig.ts
@@ -1,7 +1,7 @@
 export const corsOptions = {
   origin: (origin, callback) => {
     if (process.env.ENV === "dev" || process.env.TRUSTED_ORIGINS.includes(origin)) {
-      callback(null, true)
+      return callback(null, true)
     }
 
     callback(new Error("Not Allowed By Cors"))
