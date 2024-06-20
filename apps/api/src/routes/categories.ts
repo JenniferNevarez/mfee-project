67493680c2204728f73d528ca661ce1952f99a3a
Jennifer Nevarez <jennifer.nevarez@accenture.com>
2024-06-20 13:09:01 -0700

    refactor(api): Convert to MVC Structure

--- apps/api/src/routes/categories.ts
+++ apps/api/src/routes/categories.ts
@@ -1,59 +1,16 @@
 import express from 'express';
+import categoryController from '../controllers/category';
 
 const router = express.Router();
-const categories = []
 
 router.route('/:id')
-  .get((req, res) => {
-    const { id } = req.params;
-    const currentCategory = categories.find(el => el.id === id)
-
-    if (!currentCategory) return res.status(404).json({message: "Category not found"})
-
-    res.status(200).json(currentCategory);
-  })
-  .patch((req, res) => {
-    const { id } = req.params;
-    const categoryIndex = categories.findIndex(el => el.id === id)
-    if (categoryIndex < 0) return res.status(404).json({message: "Category not found"})
-
-    const { name } = req.body;
-    if (!name) return res.status(401).json({ message: "Name is required!" })
-
-    categories[categoryIndex].name = name;
-
-    res.status(203).json(categories[categoryIndex]);
-  })
-  .delete((req, res) => {
-    const { id } = req.params;
-    const categoryIndex = categories.findIndex(el => el.id === id)
-    if (categoryIndex < 0) return res.status(404).json({message: "Category not found"})
-
-    const currentCategory = categories[categoryIndex]
-
-    categories.splice(categoryIndex, 1)
-
-    res.status(200).json({status: "Item Deleted", item: currentCategory});
-  })
+  .get(categoryController.getCategory)
+  .patch(categoryController.updateCategory)
+  .delete(categoryController.deleteCategory)
 
 
 router.route('/')
-  .get((req, res) => {
-    res.status(200).json(categories);
-  })
-  .post((req, res) => {
-    const { name } = req.body;
-
-    if (!name) return res.status(401).json({ message: "Name is required!" })
-
-    const newCategory = {
-      id: Date.now().toString(),
-      name
-    }
-
-    categories.push(newCategory)
-
-    res.status(201).json(newCategory)
-  })
+  .get(categoryController.getAllCategories)
+  .post(categoryController.createCategory)
 
 export default router
